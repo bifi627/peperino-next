dc18dfe677b0149d76d591f2d6fc0e80a6ef29f1
bifi <dennis.kaminski95@gmail.com>
2022-03-01 22:43:45 +0100

    Demo

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ const Home: NextPage = () =>
 
             <main className={styles.main}>
                 <h1 className={styles.title}>
-                    Welcome to <a href="https://nextjs.org">Next.js! Peperino</a>
+                    Welcome to <a href="https://nextjs.org">Next.js! Demo</a>
                 </h1>
 
                 <p className={styles.description}>
